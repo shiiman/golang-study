8b8185de6cb8d40dedac7dd69c195dfcc9260eef
shiiman <hsnonsense5@gmail.com>
2024-04-12 11:08:11 +0900

    add lesson5

---
+++ lesson5/suite/calc_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+)
+
+// まず、テストスイートを作成します
+type SumSuite struct {
+	suite.Suite
+}
+
+func (suite *SumSuite) SetupSuite() {
+	suite.T().Log("SetupSuite")
+}
+
+func (suite *SumSuite) TearDownSuite() {
+	suite.T().Log("TearDownSuite")
+}
+
+func (suite *SumSuite) SetupTest() {
+	suite.T().Log("SetupTest")
+}
+
+func (suite *SumSuite) TearDownTest() {
+	suite.T().Log("TeardownTest")
+}
+
+func (suite *SumSuite) BeforeTest(suiteName, testName string) {
+	suite.T().Log(testName + ": BeforeTest")
+}
+
+func (suite *SumSuite) AfterTest(suiteName, testName string) {
+	suite.T().Log(testName + ": AfterTest")
+}
+
+// 実際のテスト(成功ケース).
+func (s *SumSuite) TestSuccess() {
+	actual := sum(3, 5)
+	expected := 8
+
+	assert.Equal(s.T(), expected, actual)
+}
+
+// 実際のテスト(失敗ケース).
+func (s *SumSuite) TestFail() {
+	actual := sum(3, 5)
+	expected := 7
+
+	assert.NotEqual(s.T(), expected, actual)
+}
+
+// テストを実行する.
+func TestSum(t *testing.T) {
+	suite.Run(t, new(SumSuite))
+}

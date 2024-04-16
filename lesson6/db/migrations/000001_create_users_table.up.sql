9b652496f562831618c46cb007127a4e351557b7
shiiman <hsnonsense5@gmail.com>
2024-04-16 11:37:29 +0900

    add lesson6

---
+++ lesson6/db/migrations/000001_create_users_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `users` (
+  `id` varchar(255) NOT NULL COMMENT 'ユーザID',
+  `username` varchar(255) NOT NULL COMMENT '名前',
+  `email` varchar(255) NOT NULL COMMENT 'メール',
+  `created_at` datetime NOT NULL COMMENT '作成日時',
+  `updated_at` datetime NOT NULL COMMENT '更新日時',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザテーブル';

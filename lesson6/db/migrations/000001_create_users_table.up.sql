a172c598475d869e6f85840b96c130a90a057dd0
shiiman <hsnonsense5@gmail.com>
2024-04-16 18:14:00 +0900

    add lesson6

--- lesson6/db/migrations/000001_create_users_table.up.sql
+++ lesson6/db/migrations/000001_create_users_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE `users` (
   `email` varchar(255) NOT NULL COMMENT 'メール',
   `created_at` datetime NOT NULL COMMENT '作成日時',
   `updated_at` datetime NOT NULL COMMENT '更新日時',
+  `deleted_at` datetime DEFAULT NULL COMMENT '削除日時',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='ユーザテーブル';
---
+++ lesson6/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+
+	"lesson6/models"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "admin_user:admin@tcp(localhost:3306)/migrate_test?parseTime=true")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// sqlboiler用の設定
+	boil.SetDB(db)
+	ctx := context.Background()
+
+	// user登録.
+	/* user := models.User{
+		ID:       "1",
+		Username: "test_user",
+		Email:    "test@email",
+	}
+	err = user.Insert(ctx, db, boil.Infer())
+	if err != nil {
+		log.Fatal(err)
+	} */
+
+	// user更新.
+	/* user, err := models.FindUser(ctx, db, "1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(user)
+
+	user.Username = "update_user"
+	_, err = user.Update(ctx, db, boil.Infer())
+	if err != nil {
+		log.Fatal(err)
+	} */
+
+	// user削除.
+	user, err := models.FindUser(ctx, db, "1")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(user)
+
+	_, err = user.Delete(ctx, db, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// usersテーブルから全ユーザーを取得.
+	users, err := models.Users().All(ctx, db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", users)
+}
